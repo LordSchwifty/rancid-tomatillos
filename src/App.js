719b5496fa4e19987290d93674a71737c9274561
Tom Doder <tdoder89@gmail.com>
2023-03-25 16:54:54 -0500

    add home button functionality

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import movieData from './movieData.js'
 import React, { Component } from 'react'
 import Movie from "./Movie";
 import SelectedMovie from "./SelectedMovie";
-import {fetchMovies} from "./apiCalls"
-const movieIndiv = {"movie": {id: 1, title: "Fake Movie Title", poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", release_date: "2019-12-04", overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", average_rating: 6, genres: ["Drama"], budget:63000000, revenue:100853753, runtime:139, tagline: "It's a movie!" }}
+import {fetchMovies, fetchMovieDetails} from "./apiCalls"
+// const movieIndiv = {"movie": {id: 1, title: "Fake Movie Title", poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", release_date: "2019-12-04", overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", average_rating: 6, genres: ["Drama"], budget:63000000, revenue:100853753, runtime:139, tagline: "It's a movie!" }}
 let movieList 
+let movieDetails
 
 class App extends Component {
   constructor() {
@@ -26,14 +27,25 @@ componentDidMount() {
     this.setState({ movies: movieList})
   })
 }
+
 //First step is to be able to click on a movie
 //render the information of the clicked movie
 //unrender all other movies
 
   selectPoster = (id) => {
-    console.log('this is id', id)
-    {this.state.movie ?  this.setState({movie: ""}):  this.setState({movie: movieIndiv.movie})}
+    fetchMovieDetails(id)
+    .then((data) => {
+      movieDetails = data;
+    })
+    .then(() => {
+      this.setState({ movie: movieDetails.movie})
+    })
   
+    // console.log('this is id', id)
+    // {this.state.movie ?  this.setState({movie: ""}):  this.setState({movie: movieIndiv.movie})}
+  }
+  homeButton = () => {
+    this.setState({ movie: ""});
   }
 
   render(){
@@ -44,7 +56,7 @@ componentDidMount() {
         {/* <Movie movieData={this.state.movies}/> */}
         {/* {this.state.movie && <h2>{this.state.movie}</h2>} */}
         {/* <SelectedMovie movieInfo={this.state.movie} selectPoster={this.selectPoster}/> */}
-        {this.state.movie ? <SelectedMovie movieInfo={this.state.movie} selectPoster={this.selectPoster}/> :  <Movie movieData={this.state.movies} selectPoster={this.selectPoster}/>}
+        {this.state.movie ? <SelectedMovie movieInfo={this.state.movie} homeButton={this.homeButton}/> :  <Movie movieData={this.state.movies} selectPoster={this.selectPoster}/>}
       </header>
     </div>
   );
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -9,5 +9,15 @@ function fetchMovies() {
     }
   )
 }
+function fetchMovieDetails(id) {
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`).then((response) => {
+        if(response.ok){
+            return response.json();
+        } else {
+            throw new Error(`${response.status} ${response.statusText}`)
+        }
+     }
+    )
+} 
 
-export { fetchMovies }
+export { fetchMovies, fetchMovieDetails }
