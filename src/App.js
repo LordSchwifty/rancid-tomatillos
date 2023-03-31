c4aa483e2b2c14dea0546e36bc3c5fe2b8836340
Gus Deribeaux <Gusderibeaux@gmail.com>
2023-03-31 18:52:18 -0400

    Add Switch

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Movie from "./Movie";
 import SelectedMovie from "./SelectedMovie";
 import {fetchMovies, fetchMovieDetails} from "./apiCalls"
 import Poster from "../src/Poster"
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 // const movieIndiv = {"movie": {id: 1, title: "Fake Movie Title", poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", release_date: "2019-12-04", overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", average_rating: 6, genres: ["Drama"], budget:63000000, revenue:100853753, runtime:139, tagline: "It's a movie!" }}
 let movieList 
 let movieDetails
@@ -41,11 +41,13 @@ componentDidMount() {
   return (
     <div className="App">
         <h1>Rancid Tomatillos</h1>
+        <Switch>
         <Route exact path="/" render={() => <Movie movieData={this.state.movies} error={this.state.error}/>}/>
         <Route exact path="/:id" render={({match}) => {
          return <SelectedMovie id={match.params.id} />
         }}
         />
+        </Switch>
     </div>
   );
 }
