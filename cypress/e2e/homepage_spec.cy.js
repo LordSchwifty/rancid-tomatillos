5e22924bada7b3339d3b79c3071714a1af758ee8
Gus Deribeaux <Gusderibeaux@gmail.com>
2023-04-02 14:48:20 -0400

    Added Single Movie Cypress testing

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -1,6 +1,6 @@
 describe('homepage', () => {
     beforeEach(() => {
-     cy.visit('http://localhost:3000/')
+     cy.visit('https://lordschwifty.github.io/rancid-tomatillos')
     })
     it('should show title', () => {
      cy.contains('h1','Rancid Tomatillos')
@@ -8,16 +8,17 @@ describe('homepage', () => {
     it('should show all movies', () => {
      cy.get('div[class=movie-container]')
      .children().should('have.length', 40)
-    })
-    it('should be able to select a movie and navigate to info', () => {
-     cy.get('div[class=poster]').contains('Black Adam')
-     .click()
-     .get('div[class=selected-movie-container]')
-     .get('p')
-     .contains('The world needed a hero. It got Black Adam.')
-     .get('button').click()
-     .get('div[class=movie-container]')
-    })
-
+    // })
+    // it('should be able to select a movie and navigate to info', () => {
+    //  cy.get('div[class=poster]').contains('Black Adam')
+    //  .click()
+    //  .get('div[class=selected-movie-container]')
+    //  .get('p')
+    //  .contains('The world needed a hero. It got Black Adam.')
+    //  .get('button').click()
+    //  .get('div[class=movie-container]')
+    // })
+  
+ })
   })
   //this shows cypress testing for our current state!
